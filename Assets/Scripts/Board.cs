d74c1ed021fee96d7f9119dcf217170313e10362
Byron Custodio <byron.custodio@gmail.com>
2017-03-12 21:17:13 -0700

    Updating build to mac

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -8,15 +8,20 @@ public class Board : MonoBehaviour {
     public GameObject cellPrefab;
     // Reference to all cell objects on the board
     public List<GameObject> cells;
+    // Reference to all Cards on the board
+    public List<Card> cardList;
     // Enumerator to determine size of board
     public enum Matrix
     {
         ThreeByThree,
         FourByFour,
         FiveByFive,
-        SixBySix
+        SixBySix,
+        EightByEight
     };
     public Matrix matrix;
+    // Distance between each cell
+    public float distance;
     // References to width and height of board
     public int width;
     public int height;
@@ -42,9 +47,13 @@ public class Board : MonoBehaviour {
                 width = 6;
                 height = 6;
                 break;
+            case "EightByEight":
+                width = 8;
+                height = 8;
+                break;
             default:
-                width = 5;
-                height = 5;
+                width = 8;
+                height = 8;
                 break;
         };
     }
@@ -52,7 +61,7 @@ public class Board : MonoBehaviour {
     /// <summary>
     /// Create the board, instantiating cells for each position of the board
     /// </summary>
-	public void Create()
+	public IEnumerator Create()
     {
         for (int i = 0; i < width; i++)
         {
@@ -61,8 +70,15 @@ public class Board : MonoBehaviour {
                 GameObject cell = Instantiate(cellPrefab, Vector3.zero, Quaternion.identity, transform) as GameObject;
                 cells.Add(cell);
                 cell.name = i + "," + j;
-                cell.GetComponent<RectTransform>().localPosition = new Vector2(i * 50, j * 50);
+
+                // Offset cell so it's centered with Camera
+                // Divide by 2 to offset board by only half the size of the board
+                float offsetX = ((width - 1) * distance) / 2;
+                float offsetY = ((height - 1) * distance) / 2;
+                cell.GetComponent<RectTransform>().localPosition = new Vector2(i * distance - offsetX, j * distance - offsetY);
             }
         }
+
+        yield return null;
     }
 }
--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -6,6 +7,37 @@ public class Card : MonoBehaviour {
 
     // Reference of card's in-game id
     public int id;
-    // Reference of card's attack[top, right, bottom, left]
-	public List<int> attack;
+    // Reference of card's owner
+    public Transform owner;
+    // Reference of card's sprite
+    public Sprite sprite;
+    // Reference of card's attack; KEYS: "top", "right", "bottom", "left"
+    public Dictionary<string, int> attack = new Dictionary<string, int>();
+    // Reference of card's effect (if it has one)
+    //public string effect;
+    public List<Text> attackUI;
+
+    public void SetOwner(Transform newOwner)
+    {
+        if (owner != newOwner)
+        {
+            owner = newOwner;
+        }
+    }
+
+    public void ShowAttackUI()
+    {
+        foreach(Text val in attackUI)
+        {
+            val.color = Color.black;
+        }
+    }
+
+    public void HideAttackUI()
+    {
+        foreach (Text val in attackUI)
+        {
+            val.color = Color.clear;
+        }
+    }
 }
